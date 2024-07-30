20a86b3bf2b734f97644091686a1af0b69462a38
Владимир <solo80@inbox.ru>
2024-07-30 10:55:16 +0300

    Закончил с задачей EqualNumber, урааа

--- src/main/java/ru/stepup/study1/EqualNumber.java
+++ src/main/java/ru/stepup/study1/EqualNumber.java
@@ -12,22 +12,25 @@ package ru.stepup.study1;
 
 public class EqualNumber {
     public static void main(String[] args) {
-        Boolean result = equalNum(77777);
+        Boolean result = equalNum(78777);
         System.out.println(result);
     }
 
     public static boolean equalNum(int x) {
-        int count = 0;;
+        int count = 0;
+        int firstNum = x % 10;
         for (int i = 0; i < x; i++) {
             count = x % 10;
             x /= 10;
+            if (firstNum == count) ;
+            else
+                return false;
         }
-        if (count == x) {
-            return true;
-        }
-        return false;
+        return true;
     }
 }
+
+
 
 
         /*int digit = x % 10;
